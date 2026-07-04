04440460fd0c62cfab736820b25d4b1b0b2a12f8
milyasmaulana70-collab <milyasmaulana70@gmail.com>
2026-07-05 00:39:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const btn=document.getElementById("openBtn");
+const btn = document.getElementById("openBtn");
+const music = document.getElementById("music");
 
-const music=document.getElementById("music");
-
-btn.onclick=()=>{
-
-music.play();
-
-alert("Welcome Alvina ❤️");
-
-}
+btn.addEventListener("click", async () => {
+    try {
+        await music.play();
+        alert("Welcome Alvina ❤️");
+    } catch (error) {
+        alert("Musik gagal diputar!");
+        console.log(error);
+    }
+});
